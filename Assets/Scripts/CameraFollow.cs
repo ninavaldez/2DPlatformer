853139dd739469e7f19b2b163a5d2d6062c08d0f
ninavaldez <78568283+ninavaldez@users.noreply.github.com>
2021-04-04 21:35:44 -0700

    Initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTransform;
+    private Vector3 smoothPos;
+    private float smoothSpeed = 0.5f;
+
+    public GameObject cameraLeftBoarder;
+    public GameObject cameraRightBoarder;
+
+    private float cameraHalfWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraHalfWidth = Camera.main.orthographicSize * Camera.main.aspect;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float boarderLeft = cameraLeftBoarder.transform.position.x + cameraHalfWidth;
+        float boarderRight = cameraRightBoarder.transform.position.x - cameraHalfWidth;
+
+        smoothPos = Vector3.Lerp(this.transform.position,
+            new Vector3(Mathf.Clamp(followTransform.position.x, boarderLeft, boarderRight),
+            this.transform.position.y,
+            this.transform.position.z), smoothSpeed);
+
+        this.transform.position = smoothPos;
+
+    }
+}
